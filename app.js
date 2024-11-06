e0c27737d7468943572e8148d74815351800c04b
dgill45 <donell.gill@outlook.com>
2024-11-06 17:23:49 -0500

    Created authRoutes.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
